c2230148f08dd568067239b81e53cea06ecee998
Ehsan Ghafoori <118250253+EhsanGhafoori@users.noreply.github.com>
2025-03-14 21:24:12 +0400

    test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,12 +9,10 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    // این متد را برای تغییرات اولیه قبل از هر تست اضافه می‌کنیم.
     public function setUp(): void
     {
         parent::setUp();
 
-        // اطمینان از اینکه کانفیگ‌ها به درستی بارگذاری می‌شوند
         Artisan::call('config:clear');
     }
 }
